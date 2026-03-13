2e87ce717a00b291a4c50af6b9d562285fc567be
Vlad <vladkuzn1604@gmail.com>
2026-03-13 20:32:36 +0300

    123

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Привет, всем!")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form.get("task")
+    print("Получена задача:", task)
+    return "Задача получена!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
